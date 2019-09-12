93f37f8490b943a02c08ffbe70eb4dc4c54a2cdf
Jonathan Allen <jallen@snow.edu>
2019-09-12 08:14:33 -0600

    Add project files.

---
+++ GuiSamples.Data/Migrations/AddLogTable.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using System;
+
+namespace GuiSamples.Data.Migrations
+{
+    [Migration(2019091201, "Add log table - first migration")]
+    public class AddLogTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Log")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Text").AsString();
+        }
+    }
+}
