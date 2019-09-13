e64f85a6284156ebfaf622dc335110e4893bb03a
Jonathan Allen <jallen@snow.edu>
2019-09-13 10:23:24 -0600

    Add IDataErrorInfo validation example

---
+++ GuiSamples/BindableDataErrorInfoBase.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace GuiSamples.Wpf
+{
+    /// <summary>
+    /// Thank you to https://stackoverflow.com/questions/3739059/prism-idataerrorinfo-validation-with-dataannotation-on-viewmodel-entities
+    /// </summary>
+    public abstract class BindableDataErrorInfoBase : BindableBase, IDataErrorInfo
+    {
+        #region IDataErrorInfo Members
+
+        public Dictionary<string, string> ErrorDictionary = new Dictionary<string, string>();
+
+        public string Error
+        {
+            get { return String.Join("; ", ErrorDictionary.Values); }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (ErrorDictionary.ContainsKey(columnName))
+                    return ErrorDictionary[columnName];
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
--- GuiSamples/ViewModels/ValidatingFormViewModel.cs
+++ GuiSamples/ViewModels/ValidatingFormViewModel.cs
@@ -5,11 +5,13 @@ using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Windows;
 
 namespace GuiSamples.Wpf.ViewModels
 {
-    public class ValidatingFormViewModel : BindableBase
+    public class ValidatingFormViewModel : BindableDataErrorInfoBase
     {
         public ValidatingFormViewModel(IRegionManager regionManager, IEventAggregator eventAggregator)
         {
@@ -42,5 +44,66 @@ namespace GuiSamples.Wpf.ViewModels
         private readonly IEventAggregator eventAggregator;
 
         public DelegateCommand<string> NavigateToMessaging { get; }
+
+        private void calculatePayment()
+        {
+            MonthlyPayment = InterestRate * LoanAmount * TermInYears;
+        }
+
+
+        private decimal interestRate;        
+        public decimal InterestRate
+        {
+            get { return interestRate; }
+            set 
+            {
+                if (value <= 0)
+                    InterestRateError = "Interest rate must be >= 0";
+                else
+                    InterestRateError = null;
+                SetProperty(ref interestRate, value);                
+                calculatePayment();
+            }
+        }
+        private decimal loanAmount;
+        public decimal LoanAmount
+        {
+            get { return loanAmount; }
+            set { SetProperty(ref loanAmount, value);
+                calculatePayment();
+            }
+        }
+        private int termInYears;
+        public int TermInYears
+        {
+            get { return termInYears; }
+            set { SetProperty(ref termInYears, value);
+                calculatePayment();
+            }
+        }
+        private decimal monthlyPayment;
+        public decimal MonthlyPayment
+        {
+            get { return monthlyPayment; }
+            set { SetProperty(ref monthlyPayment, value); }
+        }
+
+        private string interestRateError;
+        public string InterestRateError
+        {
+            get { return interestRateError; }
+            set 
+            { 
+                SetProperty(ref interestRateError, value);
+                ErrorDictionary[nameof(InterestRate)] = value;
+                InterestRateErrorVisibility = String.IsNullOrWhiteSpace(value) ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+        private Visibility interestRateErrorVisibility;
+        public Visibility InterestRateErrorVisibility
+        {
+            get { return interestRateErrorVisibility; }
+            set { SetProperty(ref interestRateErrorVisibility, value); }
+        }
     }
 }
